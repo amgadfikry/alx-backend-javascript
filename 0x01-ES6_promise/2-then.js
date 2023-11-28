596c8a3315bcc5e0f7723cddb44ad4195dd864d8
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-11-28 18:19:39 +0200

    fix function by add return statement and object

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then((res) => {
+  return promise.then((res) => { // eslint-disable-line no-unused-vars
     console.log('Got a response from the API');
-    return res;
+    return { status: 200, body: 'success' };
   })
     .catch((error) => { // eslint-disable-line no-unused-vars
       console.log('Got a response from the API');
-      return (new Error({}));
+      return new Error({});
     });
 }
