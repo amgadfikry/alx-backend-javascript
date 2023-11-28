fc4b62e661890bb79821cef7498faf81c7e8538e
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-11-28 19:00:49 +0200

    fix function by used promise allsetelled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,7 +4,9 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
-  return Promise.all([user, photo])
-    .then((res) => ({ status: 'fullfilled', value: res }))
-    .catch((error) => ({ status: 'rejected', value: error }));
+  return Promise.allSettled([user, photo])
+    .then((res) => (res.map((r) => ({
+      status: r.status,
+      value: r.status === 'fulfilled' ? r.value : r.reason,
+    }))));
 }
