2d317e27fea5e223ab083648181527c81476f910
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-11-28 18:44:32 +0200

    fix function by split response of every promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const photo = uploadPhoto();
-  const user = createUser();
-  Promise.all([photo, user])
-    .then(([photoRes, userRes]) => {
-      console.log(`${photoRes.body} ${userRes.firstName} ${userRes.lastName}`);
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoResponse, userResponse]) => {
+      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
-    .catch((error) => console.log('Signup system offline')); // eslint-disable-line no-unused-vars
+    .catch((error) => { // eslint-disable-line no-unused-vars
+      console.error('Signup system offline');
+    });
 }
