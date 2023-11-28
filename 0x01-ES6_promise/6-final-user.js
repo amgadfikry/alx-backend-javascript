4108893da263986a849454835e5fadcde95fe5b9
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-11-28 13:27:14 +0200

    function return results of all promises as list

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+  return Promise.all([user, photo])
+    .then((res) => ({ status: 'fullfilled', value: res }))
+    .catch((error) => ({ status: 'rejected', value: error }));
+}
