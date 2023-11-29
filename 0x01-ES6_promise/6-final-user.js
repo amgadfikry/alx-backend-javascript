f5ef8076eda7994d4887bf3041c724052b46cd58
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-11-29 06:13:49 +0200

    fix functions

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const user = signUpUser(firstName, lastName);
-  const photo = uploadPhoto(fileName);
-  return Promise.allSettled([user, photo])
-    .then((res) => (res.map((r) => ({
-      status: r.status,
-      value: r.status === 'fulfilled' ? r.value : r.reason,
-    }))));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
