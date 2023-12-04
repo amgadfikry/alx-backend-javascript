a2fd7d3110b483d6774572ce75a85d621d3f869d
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-12-04 18:08:16 +0200

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
-    return startString;
+  if (!startString || !set || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
   }
   const setFiltered = Array.from(set).filter((el) => el.startsWith(startString));
   const setRemoved = setFiltered.map((el) => el.slice(startString.length));
