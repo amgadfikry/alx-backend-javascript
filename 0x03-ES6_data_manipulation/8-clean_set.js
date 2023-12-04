1500854bd41b301b1ecbe092661036bce49b23b1
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-12-04 18:13:39 +0200

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (!startString || !set || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
-  const setFiltered = Array.from(set).filter((el) => (el.startsWith(startString) && typeof el === 'string'));
+  const setFiltered = Array.from(set).filter((el) => (typeof el === 'string' && el.startsWith(startString)));
   const setRemoved = setFiltered.map((el) => el.slice(startString.length));
   return setRemoved.join('-');
 }
