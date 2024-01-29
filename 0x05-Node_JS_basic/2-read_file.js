5ee17e61f2344bcfde447465a4f0b88d160614c3
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-29 22:51:52 +0200

    read data from csv and print some results

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+
+    const list = data.split('\n')
+      .slice(1)
+      .map((el) => el.split(','))
+      .filter((el) => el.length > 1);
+
+    console.log(`Number of students: ${list.length}`);
+
+    const uniqueObj = {};
+
+    list.forEach((el) => {
+      if (el[3] in uniqueObj) {
+        uniqueObj[el[3]].push(el[0]);
+      } else {
+        uniqueObj[el[3]] = [el[0]];
+      }
+    });
+
+    for (const value of Object.values(uniqueObj)) {
+      console.log(`Number of students in FIELD: ${value.length}. List: ${value.join(', ')}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
