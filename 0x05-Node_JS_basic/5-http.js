be438d1766af47dd6a22cd977d0a64d61a914a1b
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-30 01:34:16 +0200

    create small server with specific routes

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const url = require('url');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer(async (req, res) => {
+  const path = url.parse(req.url).pathname;
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (path === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  } else if (path === '/students') {
+    const name = process.argv[2];
+    const data = await countStudents(name);
+    res.write('This is the list of our students\n');
+    res.end(data.join('\n'));
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
