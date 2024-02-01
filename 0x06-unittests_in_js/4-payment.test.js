78608b60f1467b5f0b62933953437cbc6a4994d3
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-02-01 22:45:09 +0200

    use stub sion in tests

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -6,14 +6,11 @@ const expect = chai.expect;
 
 describe('sendPaymentRequestToApi', () => {
   it('test spy of sinon', () => {
-    const spy = sinon.spy(utlis, 'calculateNumber');
     const spyConsole = sinon.spy(console, 'log');
     const stub = sinon.stub(utlis, 'calculateNumber').returns(10)
     const result = sendPaymentRequestToApi(100, 20);
-    expect(spy.calledOnceWith('SUM', 100, 20)).to.be.true;
+    expect(stub.calledOnceWith('SUM', 100, 20)).to.be.true;
     expect(spyConsole.calledOnceWith('The total is: 10')).to.be.true;
-    expect(result).to.equal(10);
-    spy.restore();
     spyConsole.restore();
     stub.restore();
   });
